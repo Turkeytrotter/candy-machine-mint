7a8ee9a516c200c7ef747f8878b5af985b8294e7
freakazo1d <51143847+freakazo1d@users.noreply.github.com>
2021-12-03 11:05:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useMemo } from "react";
 
-import Home from "./Home";
+import Home from "../Home";
 import About from "../About";
 
 import * as anchor from "@project-serum/anchor";
