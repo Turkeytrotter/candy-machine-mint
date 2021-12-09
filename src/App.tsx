c379c795cd4ad558ea9467af05e95c142e90673d
Turkeytrotter <92885873+Turkeytrotter@users.noreply.github.com>
2021-12-09 11:20:58 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { useMemo } from "react";
 
-import About from "../About";
-
 import * as anchor from "@project-serum/anchor";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
